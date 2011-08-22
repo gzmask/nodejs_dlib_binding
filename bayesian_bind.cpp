fde9ea52c788d0ac402cb41fc38fc753e6bb8e33
gzmask <gzmask@gmail.com>
2011-08-22 10:56:09 -0600

    move file io out from dlib part

--- bayesian_bind.cpp
+++ bayesian_bind.cpp
@@ -1,6 +1,7 @@
 #include <v8.h>
 #include <node.h>
 #include <string.h>
+#include <fstream>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -24,35 +25,62 @@ struct simple_request {
 
 
 static Handle<Value> DoSomethingAsync (const Arguments& args) {
-  HandleScope scope;
-  const char *usage = "usage: doSomething(passed_courses, cb)";
-  if (args.Length() != 2) {
-    return ThrowException(Exception::Error(String::New(usage)));
-  }
-  String::Utf8Value passed_courses(args[0]); 
-  Local<Function> cb = Local<Function>::Cast(args[1]); //this is the callback(cb) in doSomething, which is passed by javascript
+	HandleScope scope;
+	const char *usage = "usage: doSomething(passed_courses, cb)";
+	if (args.Length() != 2) {
+		return ThrowException(Exception::Error(String::New(usage)));
+	}
+	String::Utf8Value passed_courses(args[0]); 
+	Local<Function> cb = Local<Function>::Cast(args[1]); //this is the callback(cb) in doSomething, which is passed by javascript
 
-  simple_request *sr = (simple_request *)
-    malloc(sizeof(struct simple_request) + passed_courses.length() + 1); //get memory in heap
+	simple_request *sr = (simple_request *)
+	malloc(sizeof(struct simple_request) + passed_courses.length() + 1); //get memory in heap
 
-  //assign four parameters of doSomething to struct
-  sr->cb = Persistent<Function>::New(cb);
-  strncpy(sr->passed_courses, *passed_courses, passed_courses.length() + 1);
+	//assign four parameters of doSomething to struct
+	sr->cb = Persistent<Function>::New(cb);
+	strncpy(sr->passed_courses, *passed_courses, passed_courses.length() + 1);
 
-  //deploy the c++ DoSomething/DoSomething_After function, and struct to thread pool using libeio 
-  eio_custom(DoSomething, EIO_PRI_DEFAULT, DoSomething_After, sr); //I believe sr got sent to DoSomething/DoSomething_After as eio_req *req->data in the following func def
-  ev_ref(EV_DEFAULT_UC);
-  return Undefined();
+	//deploy the c++ DoSomething/DoSomething_After function, and struct to thread pool using libeio 
+	eio_custom(DoSomething, EIO_PRI_DEFAULT, DoSomething_After, sr); //I believe sr got sent to DoSomething/DoSomething_After as eio_req *req->data in the following func def
+	ev_ref(EV_DEFAULT_UC);
+	return Undefined();
 }
 
 // this function happens on the thread pool
 // doing v8 things in here will make bad happen.
 static int DoSomething (eio_req *req) {
-  struct simple_request * sr = (struct simple_request *)req->data;
-  sr->bayesian_result = bayesian_test(sr->passed_courses);
-  return 0;
+	struct simple_request * sr = (struct simple_request *)req->data;
+	char line_str[256];
+	char block_str[4096];
+	char xbn_filename[] = "input.xbn";
+
+	//this is the code block that reads the ???.xbn file
+
+	std::ifstream xbn_file (xbn_filename);
+	if (xbn_file.is_open()) {
+		xbn_file.getline(line_str, 256);
+		strcpy(block_str, line_str);
+		while (!xbn_file.eof()) {
+			xbn_file.getline(line_str, 256);
+			//line_str need to be examine throughoutly 
+			//cout<<"line_str size:"<<line_str.size()<<endl;
+			//for (int i=0; i< line_str.size(); i++) {
+				//cout<<line_str[i];
+				//printf("%c\n", line_str[i]);
+			//}
+			strcat(block_str, line_str);
+		}
+		//cout<<block_str<<endl;
+		xbn_file.close();
+	}
+	printf("the length of block_str is: %d \n",strlen(block_str));
+	//printf(block_str);
+
+	sr->bayesian_result = bayesian_test(sr->passed_courses);
+	return 0;
 }
 
+/*Static member functions have a class scope and they do not have access to the 'this' pointer of the class. When a member is declared as static, a static member of class, it has only one data for the entire class even though there are many objects created for the class. The main usage of static function is when the programmer wants to have a function which is accessible even when the class is not instantiated.*/
 
 //the callback function cb, passed by javascript, happens after everything
 //in this case, callback(er = null, res = x + y, n = name), which are argv[0], argv[1] and argv[2] 
